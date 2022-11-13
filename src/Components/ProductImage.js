3007faea2b353ae9ff85c705f6885935aa19acec
Honko-o <98553562+Honko-o@users.noreply.github.com>
2022-11-13 13:49:22 +0200

    Update ProductImage.js

--- src/Components/ProductImage.js
+++ src/Components/ProductImage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import mobileImage from '../images/image-product-mobile.jpg';
 import pcImage from '../images/image-product-desktop.jpg';
@@ -36,42 +35,3 @@ class ProductImage extends Component {
 }
 
 export default ProductImage;
-=======
-import React, { Component } from 'react';
-import mobileImage from '../images/image-product-mobile.jpg';
-import pcImage from '../images/image-product-desktop.jpg';
-class ProductImage extends Component {
-    state = { windowWidth: window.innerWidth };
-
-    onWindowResize = () => {
-        this.setState({ windowWidth: window.innerWidth });
-    };
-
-    componentDidMount() {
-        window.addEventListener('resize', this.onWindowResize);
-    }
-
-    // Event Listeners Cleanup
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.onWindowResize);
-    }
-
-    render() {
-        if (this.state.windowWidth >= 680) {
-            return (
-                <div className="product-image">
-                    <img src={pcImage} alt="perfume img" />
-                </div>
-            );
-        }
-
-        return (
-            <div className="product-image">
-                <img src={mobileImage} alt="perfume img" />
-            </div>
-        );
-    }
-}
-
-export default ProductImage;
->>>>>>> f28c227637aa20f2e08ebb00b73be7215969256a
